2e6b1150f7c2e79c89d7467f9b8abf92fc484284
Pablo Daniel Acosta Morales <acostamoralespablodaniel@gmail.com>
2025-10-19 01:58:28 -0600

    Se agrego la colleccion a la base de datos

--- server/src/models/Temporizador.js
+++ server/src/models/Temporizador.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 const temporizadorSchema = new mongoose.Schema({
     fechaLimite: { type: Date, required: true },
     imagen: {type: String, required: true },
+}, {
+    collection: 'temporizadors' // Mantiene el nombre que ya existe en MongoDB
 });
 
 export default mongoose.model('Temporizador', temporizadorSchema);
