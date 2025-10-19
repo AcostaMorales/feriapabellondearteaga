f74d99719188f5cebae922d550c70766562be1c1
Pablo Daniel Acosta Morales <acostamoralespablodaniel@gmail.com>
2025-10-19 03:29:14 -0600

    intento de arreglar la pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -23,18 +23,36 @@ export default defineConfig({
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any'
           },
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+        screenshots: [
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            form_factor: 'narrow',
+            label: 'Feria de Pabellón - Vista móvil'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512', 
+            type: 'image/png',
+            form_factor: 'wide',
+            label: 'Feria de Pabellón - Vista escritorio'
           }
         ]
       },
