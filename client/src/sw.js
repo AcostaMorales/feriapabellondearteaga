67c971de110905bd9dabcb01b31e8cb65503cab5
Pablo Daniel Acosta Morales <acostamoralespablodaniel@gmail.com>
2025-11-21 01:19:54 -0600

    Se Arreglo el link del mapa

--- client/src/sw.js
+++ client/src/sw.js
@@ -1,7 +1,7 @@
 import { precacheAndRoute } from 'workbox-precaching';
 
 // Versión del Service Worker para forzar actualizaciones
-const CACHE_VERSION = 'v1.7.9';
+const CACHE_VERSION = 'v1.7.10';
 console.log('Service Worker iniciado -', CACHE_VERSION);
 
 // Precache de archivos estáticos
